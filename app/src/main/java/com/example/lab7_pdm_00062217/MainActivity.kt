f70c263baec7a71e99380387205478683304465b
RFuentes22 <00062217@uca.edu.sv>
2019-05-17 10:03:25 -0600

    First commit

---
+++ app/src/main/java/com/example/lab7_pdm_00062217/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lab7_pdm_00062217
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
